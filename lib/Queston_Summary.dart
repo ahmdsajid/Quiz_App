9c21b5d24fe3e3f2a88aff484364962d9856af10
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2024-10-28 00:20:50 +0600

    update

--- lib/Queston_Summary.dart
+++ lib/Queston_Summary.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:quiz_app/Quiz.dart';
 
 class QuestionSummary extends StatelessWidget {
   const QuestionSummary({super.key, required this.summaryData});
